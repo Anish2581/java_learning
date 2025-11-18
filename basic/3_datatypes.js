a875bcf1a7d2a2d0f54ec9cd81910385c1a0e551
Anish2581 <figmaani@gmail.com>
2025-11-18 14:41:16 +0000

    frist commit using command line and changing frist commit using command line

--- basic/3_datatypes.js
+++ basic/3_datatypes.js
@@ -13,4 +13,62 @@ console.log(typeof d)//object
 console.log(typeof e)//undefined
 console.log(typeof f)//number
                      //"bigint" for long numbers
-console.log(e)//string
+console.log(e)//undefined
+//two types of datatype is there 
+//PRIMITIVES DATATYPE(call by value)
+//7 types: string,number,boolean,null,undefined,symbol,bigInt
+
+
+//NON-PRIMITIVES DATATYPE(call by Refrences)
+//Arrays,object,function
+
+//symbol
+const id=Symbol('123')
+const aid=Symbol('123')
+console.log(id==aid)
+console.log(id ,aid)
+
+//bigInt
+let bigInt=636356383564783873734847n//when we attach "n" after any number it will considered it as bigInt
+let nbigInt=7575n//type of this variable is "bigInt" 
+
+
+//arrays
+const students=["anish","harsh","akash","aryan"]
+console.log(students)
+
+//object
+
+let myobj={
+    name:"anish",
+    rollno:"213"
+}
+console.log(typeof(myobj))
+console.log(myobj)
+
+
+//function
+const myfunc=function(){
+    console.log("hello world")
+}
+console.log(myfunc)
+console.log(typeof myfunc)
+
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+//stack(primitive),heap(non-primitive)
+let one="anish"
+let two=one
+two="aryan"
+console.log(one)//by changing variable "two" variable "one" is not change "meaning it is pass by value"
+console.log(two)//"Deep Copy"
+
+
+let obj={
+    name:"anish",
+    email:"anish.com"
+}
+let obj1=obj
+obj1.email="gmail.com"
+console.log(obj)//by changing "obj1" "obj" is also changed cuz it is "pass by reference"
+console.log(obj1)//"shallow copy"
---
+++ basic/6_Comparison.js
@@ -0,0 +1,27 @@
+console.log("1.",2>1)//true
+console.log("2.",2>=1)//true
+console.log("3.",2<1)//false
+console.log("4.",2<=1)//false
+console.log("5.",2==1)//false
+
+
+console.log("\n")
+
+
+console.log("1.","2"==2)//true
+console.log("2.",2=="02")//true
+console.log("3.",2>="02")//true
+console.log("4.",2<="02")//true    //whenever we starts with string it will convert all further element into string
+console.log("5.",2<"02")//false
+console.log("6.","02"<=2)//true
+console.log("7.","02">=2)//true
+console.log("8.","02">2)//false
+console.log("9.","02"==2)//true
+
+
+console.log("\n")
+
+
+console.log(null==0)
+console.log(null<=0)
+console.log(null>=0)
